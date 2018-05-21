352c4b4777df4dc3200ea2e0d203b1a9ee264888
Walter Michelin <wmichelin@gmail.com>
2018-05-20 17:55:54 -0700

    Move bulk of logic to lib folder

--- main.py
+++ main.py
@@ -1,38 +1,4 @@
-import requests
-
-from constants import (
-    BASE_URL,
-    COMMAND_PREDICTION,
-    AGENCY,
-    DEFAULT_STOPS
-)
-
-def build_prediction_url(stops=DEFAULT_STOPS):
-    return (
-        BASE_URL + '?' +
-        'a=' + AGENCY + '&' +
-        'command=' + COMMAND_PREDICTION + '&' +
-        build_stops_str(stops)
-    )
-
-def build_stops_str(stops=DEFAULT_STOPS):
-    stops_str = ''
-    for idx, stop in enumerate(stops):
-        stops_str += (
-            'stops=' +
-            stop.get('routeTag') + '|' +
-            stop.get('tag')
-        )
-        if idx is not (len(stops) - 1):
-            stops_str += '&'
-
-    return stops_str
-
-
-def get_predictions():
-    url = build_prediction_url()
-    return requests.get(url).json().get('predictions')
-
+from muni.lib import get_predictions
 
 for result in get_predictions():
     print(result.get('routeTitle'))
@@ -49,6 +15,6 @@ for result in get_predictions():
 
         for prediction in predictions:
             seconds = float(prediction.get('seconds'))
-            minutes = seconds / 60
+            minutes = round(seconds / 60, 2)
             print(str(minutes) + ' minutes')
     print('------------------------------------------')
